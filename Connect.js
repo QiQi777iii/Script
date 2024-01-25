1555747aa93dc35cf589cfc3f46183ed29b48525
QiQi777iii <141505720+QiQi777iii@users.noreply.github.com>
2024-01-25 19:38:14 +0800

    Add files via upload

---
+++ Connect.js
@@ -0,0 +1,34 @@
+let $ = {
+    Google: 'https://www.google.com/generate_204',
+    Github: 'https://www.github.com',
+    PikPak: 'https://mypikpak.com/',
+    Rule34: 'https://rule34video.com',
+    MissAV: 'https://missav.com/cn',
+    Ehentai: 'https://e-hentai.org/'
+}
+
+!(async () => {
+    let results = await Promise.allSettled([
+        http('Google'), http('Github'), http('PikPak'), http('MissAV'), http('Rule34'), http('Ehentai')
+    ]).then(results => results.map((result, index) => {
+        // 每两个结果后面加一个换行符，除非是最后一个结果
+        return result.value + ((index % 2) && index !== results.length - 1 ? '\n' : '\t');
+    }));
+
+    $done({
+        title: 'Network Connectivity Test',
+        content: results.join(''), // 已经在上面加了换行，这里不需要再加
+        icon: 'timer',
+        'icon-color': '#FF5A9AF9',
+    });
+})();
+
+function http(req) {
+    return new Promise((resolve) => {
+        let time = Date.now();
+        $httpClient.post($[req], (err, resp, data) => {
+            let resultString = req + ' : ' + (Date.now() - time) + ' ms';
+            resolve(resultString);
+        });
+    });
+}
